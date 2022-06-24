c65fd8bb037d3e6672fe97d95577ce50675bf6f8
Alexander Cobble <acobble@Alexanders-MacBook-Pro.local>
2022-06-24 07:00:36 -0700

    Created a red and black checker board by only manipulating the DOM inside JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 // Your JS goes here
+
+function redBox(){
+    
+    let container = document.createElement('div')
+    document.body.append(container);
+    
+    for(let i = 0; i < 8; i++){
+        for(let j=0; j< 8; j++){
+            let div = document.createElement('div');
+            if(i % 2 === 0){
+                if(j % 2 === 0){   
+                    boxProperties(div);
+                    div.style.backgroundColor = 'red';
+                    container.append(div);
+                }else{
+                    boxProperties(div);
+                    div.style.backgroundColor = 'black';
+                    container.append(div)
+            }
+        }else{
+            if(j % 2 === 0){ 
+                boxProperties(div);
+                div.style.backgroundColor = 'black';
+                container.append(div);
+            }else{
+                boxProperties(div);
+                div.style.backgroundColor = 'red';
+                container.append(div)
+            }
+        }
+        }
+    }
+}
+
+function boxProperties(div){
+    
+    div.style.border = '1px solid black';
+    div.style.width = "12.0%"; 
+    div.style.float = "left"; 
+    div.style.paddingBottom = "12.0%";
+}
+redBox()
