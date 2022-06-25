18684a1d8c81ead9f887d4eb8f527de8bf1b7a46
Alexander Cobble <acobble@Alexanders-MacBook-Pro.local>
2022-06-25 14:25:05 -0700

    created a new div and added a gradient over entire checkerboard.

--- script.js
+++ script.js
@@ -2,9 +2,8 @@
 
 function redBox(){
     
-    let container = document.createElement('div')
+    let container = document.createElement('div');
     document.body.append(container);
-    
     for(let i = 0; i < 8; i++){
         for(let j=0; j< 8; j++){
             let div = document.createElement('div');
@@ -16,7 +15,7 @@ function redBox(){
                 }else{
                     boxProperties(div);
                     div.style.backgroundColor = 'black';
-                    container.append(div)
+                    container.append(div);
             }
         }else{
             if(j % 2 === 0){ 
@@ -26,18 +25,24 @@ function redBox(){
             }else{
                 boxProperties(div);
                 div.style.backgroundColor = 'red';
-                container.append(div)
+                container.append(div);
             }
         }
         }
     }
+     let gradient = document.createElement('div');
+     gradient.style.backgroundImage ='linear-gradient(rgba(0,0,255,1), rgba(255,0,0,0.7))';
+     gradient.style.paddingBottom = '100%';
+     gradient.style.width = '100%';
+     gradient.style.opacity = '75%';
+     container.append(gradient);
 }
 
 function boxProperties(div){
     
-    div.style.border = '1px solid black';
-    div.style.width = "12.0%"; 
+    div.style.width = "12.5%"; 
     div.style.float = "left"; 
-    div.style.paddingBottom = "12.0%";
+    div.style.paddingBottom = "12.5%";
 }
+
 redBox()
