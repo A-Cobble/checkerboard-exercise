d54988724703fcea52c2be072a37f55d8c339a53
Alexander Cobble <acobble@Alexanders-MacBook-Pro.local>
2022-06-27 06:35:38 -0700

    Created random colors in each tile. removed unnecessary if and if else statements as well as the nested for loop. Created an array to hold my divs so that setInterval could use a for loop to change the tile color every 2 seconds.

--- script.js
+++ script.js
@@ -1,43 +1,36 @@
 // Your JS goes here
-
+var arrayOfTiles = []
 function redBox(){
-    
     let container = document.createElement('div')
     document.body.append(container);
     
-    for(let i = 0; i < 8; i++){
-        for(let j=0; j< 8; j++){
-            let div = document.createElement('div');
-            if(i % 2 === 0){
-                if(j % 2 === 0){   
-                    boxProperties(div);
-                    div.style.backgroundColor = 'red';
-                    container.append(div);
-                }else{
-                    boxProperties(div);
-                    div.style.backgroundColor = 'black';
-                    container.append(div)
-            }
-        }else{
-            if(j % 2 === 0){ 
-                boxProperties(div);
-                div.style.backgroundColor = 'black';
-                container.append(div);
-            }else{
-                boxProperties(div);
-                div.style.backgroundColor = 'red';
-                container.append(div)
-            }
-        }
-        }
+    for(let i = 0; i < 64; i++){
+        let div = document.createElement('div');
+        arrayOfTiles.push(div)
+        boxProperties(div);
+        div.style.backgroundColor = randomColors();
+        container.append(div);
     }
 }
 
 function boxProperties(div){
-    
     div.style.border = '1px solid black';
-    div.style.width = "12.0%"; 
+    div.style.width = "12%"; 
     div.style.float = "left"; 
-    div.style.paddingBottom = "12.0%";
+    div.style.paddingBottom = "12%";
 }
-redBox()
+
+function randomColors(){
+    var red = Math.floor((Math.random()* 255) + 1);
+    var green =Math.floor((Math.random()* 255) + 1);
+    var blue = Math.floor((Math.random()* 255) + 1);
+    return `rgb(${red},${green},${blue})`
+}
+
+setInterval(function(){
+    for(let i =0; i<arrayOfTiles.length;i++){
+        arrayOfTiles[i].style.backgroundColor = randomColors();
+    }
+}, 2000);
+
+redBox();
